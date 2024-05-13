4d2825523e010ac4af8641e302614df598f5aa09
Dcarrasc <David.carrascoo@usm.cl>
2024-05-13 16:04:22 -0400

    Se agregan comentarios
    Acerca de lo que contiene el index.js

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -4,7 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+//Index.js es el punto de entrada sobre el cual se cargan distintos componentes al DOM
+
+//Esta es la web dinamica, identifica los elementos
 const root = ReactDOM.createRoot(document.getElementById('root'));
+//Renderizar es generar una instancia que se pueda mostrar en pantalla que tiene relación con el contenido dinamico ques se genera a través de los componentes.
 root.render(
   <React.StrictMode>
     <App />
